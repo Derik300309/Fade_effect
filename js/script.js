e12b16161723e1d45bbf0e4b514206d6c7a67e21
Derik300309 <derik3009@gmail.com>
2021-02-19 20:59:04 +0500

    he final version

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
-$(window).on("load", function () {
-  $(window).scroll(function () {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade").each(function () {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
+$(document).ready(function () {
 
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity") == 0) { $(this).fadeTo(500, 1); }
-      } else { //object goes out of view (scrolling up)
-        if ($(this).css("opacity") == 1) { $(this).fadeTo(500, 0); }
+  // Whenever the window is scrolled ... 
+  $(window).scroll(function () {
+
+    // Check the location of the object
+    $('.fade-in').each(function (i) {
+
+      // Find the location of the objects
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+
+      // Find out how far we've scrolled down
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      // If we have scrolled past the bottom of the object,
+      // fade it in!
+      if (bottom_of_window > bottom_of_object) {
+
+        $(this).animate({ 'opacity': '1' }, 1000);
       }
     });
-  }).scroll(); //invoke scroll-handler on page-load
-});
+  });
+})
